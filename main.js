a165500a7736326199c819376092f3c2db163fda
Daniil-Saukou <133404813+Daniil-Saukou@users.noreply.github.com>
2023-06-09 21:49:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let width = window.innerWidth - 140;
+let height = window.innerHeight - 100;
+
+function run(){
+	let top = Math.random() * height;
+	let left = Math.random() * width;
+
+	document.getElementById('theRunButton').style.top = top + 'px';
+	document.getElementById('theRunButton').style.left = left + 'px';
+}
+
+function correctMessage(){
+	alert('это отличный ответ!');
+}
+
+function badMessage(){
+	alert('это неправильный ответ!');
+}
+
